2e4293df4db81d8837e77485953876e06eafbfa1
Adam Cohn <adamcohn88@gmail.com>
2018-07-17 23:04:00 -0400

    much faster querying

--- app/models/state.rb
+++ app/models/state.rb
@@ -17,43 +17,52 @@ class State < ApplicationRecord
   end
 
   def render_counties
-    major_candidates = candidates.group('candidates.id').limit(5).order('sum_total DESC').sum(:total).keys
-    to_list = Candidate.find(major_candidates)
+    major_candidates = candidates.group('candidates.id').limit(5).order('sum_total DESC').sum(:total)
+    candidates = Candidate.find(major_candidates.keys).to_a.map(&:serializable_hash).sort_by { |h| h['id'] }
     { name: name,
       fips: fips,
-      counties:
-      counties.map do |county|
-        # major_candidate_results = county.candidates.group('candidates.normalized_name').limit(5).order('sum_total DESC').sum(:total)
-        # major_candidate_totals = major_candidate_results.values.inject(0) { |sum, n| sum + n }
-        # major_candidate_results['other'] = county.results.sum(:total) - major_candidate_totals
+      counties: counties.map do |county|
+        results = County.joins(:candidates).where(candidates: {id: major_candidates.keys}, counties: {id: county.id}).group('candidates.id').sum(:total).values
+        candidate_results = candidates.zip(results)
         {
           county_name: county.name,
           county_fips: county.fips,
-          results:  to_list.map do |candidate|
+          results: candidate_results.map do |candidate|
             {
-            name: candidate.name,
-            party: candidate.party,
-            total: candidate.results.where(county: county).sum(:total)
-            }
-          end
+            name: candidate[0]['name'],
+            normalized_name: candidate[0]['normalized_name'],
+            fec_id: candidate[0]['fec_id'],
+            party: candidate[0]['party'],
+            total: candidate.last
+          }
+        end
         }
       end
-    }
-  end
+      }
+    end
 
   def render_precincts
+    major_candidates = candidates.group('candidates.id').limit(5).order('sum_total DESC').sum(:total)
+    candidates = Candidate.find(major_candidates.keys).to_a.map(&:serializable_hash).sort_by { |h| h['id'] }
     { name: name,
       fips: fips,
-      precincts:
-      precincts.map do |precinct|
-        major_candidate_results = precinct.candidates.group('candidates.normalized_name').limit(5).order('sum_total DESC').sum(:total)
-        major_candidate_totals = major_candidate_results.values.inject(0) { |sum, n| sum + n }
-        major_candidate_results['other'] = precinct.results.sum(:total) - major_candidate_totals
+      precincts: precincts.map do |precinct|
+        results = Precinct.joins(:candidates).where(candidates: {id: major_candidates.keys}, precincts: {id: precinct.id}).group('candidates.id').sum(:total).values
+        candidate_results = candidates.zip(results)
         {
           precinct_name: precinct.name,
-          results:  [major_candidate_results]
+          results: candidate_results.map do |candidate|
+            {
+            name: candidate[0]['name'],
+            normalized_name: candidate[0]['normalized_name'],
+            fec_id: candidate[0]['fec_id'],
+            party: candidate[0]['party'],
+            total: candidate.last
+          }
+        end
         }
-      end }
+      end
+      }
     end
 
   def render_show
