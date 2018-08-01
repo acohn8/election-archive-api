cf3d15c544c0ebee69299ea5a50b9c4a4c15c257
Adam Cohn <adamcohn88@gmail.com>
2018-08-01 09:37:47 -0400

    before exports

--- app/models/state.rb
+++ app/models/state.rb
@@ -52,7 +52,7 @@ class State < ApplicationRecord
     precinct_results.keys.each do |precinct_id|
       precinct_results[precinct_id] ||= [:other]
       precinct_results[precinct_id][:other] ||= other_results[precinct_id].to_i
-      formatted_hash <<  Hash[id: precinct_id, county_id: candidate_results.find { |p| p.precinct_id == precinct_id }.county_id, results: precinct_results[precinct_id]]
+      formatted_hash << Hash[id: precinct_id, county_id: candidate_results.find { |p| p.precinct_id == precinct_id }.county_id, results: precinct_results[precinct_id]]
     end
      { results: formatted_hash }
   end
@@ -61,7 +61,7 @@ class State < ApplicationRecord
     { name: name,
       fips: fips,
       results:  candidates.distinct.map do |candidate|
-        {
+       {
           name: candidate.name,
           normalized_name: candidate.normalized_name,
           fec_id: candidate.fec_id,
@@ -83,7 +83,7 @@ class State < ApplicationRecord
     state_candidates = candidates.distinct.to_a
     other_results.delete_if { |k, v| !county_results.include?(k) }
     county_results.keys.each do |county_id|
-      county = state_counties.find{ |c| c.id == county_id }
+      county = state_counties.find { |c| c.id == county_id }
       candidate_totals = county_results[county_id].transform_keys { |k| state_candidates.find { |c| c.id == k }.name }
       candidate_totals[:other] ||= other_results[county_id]
       result = { county: county.name, fips: county.fips }.merge(candidate_totals)
@@ -98,7 +98,7 @@ class State < ApplicationRecord
     export
   end
 
-    def precinct_results_export
+  def precinct_results_export
     formatted_hash = []
     top_three = results.includes(:candidate).group('candidates.id').sum(:total).sort{|a,b| a[1]<=>b[1]}.reverse[0..2].map{|k, v| k }
     candidate_results = results.includes(:precinct, :candidate)
@@ -111,14 +111,14 @@ class State < ApplicationRecord
     other_results.delete_if { |k, v| !precinct_results.include?(k) }
     precinct_results.keys.each do |precinct_id|
       precinct = state_precincts.find { |p| p.id == precinct_id }
-      county = state_counties.find { |c| c.id == precinct.county_id}
+      county = state_counties.find { |c| c.id == precinct.county_id }
       candidate_totals = precinct_results[precinct_id].transform_keys { |k| state_candidates.find { |c| c.id == k }.name }
       candidate_totals[:other] ||= other_results[precinct_id]
       result = { precinct: precinct.name, precinct_county: county.name, county_fips: county.fips }.merge(candidate_totals)
       formatted_hash << result
     end
 
-    export = generated_csv = CSV.generate do |csv|
+    export = CSV.generate do |csv|
       csv << formatted_hash.first.keys
       formatted_hash.each do |county|
         csv << county.values
