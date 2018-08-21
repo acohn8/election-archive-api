0cc1c66d10e814a36f66dc94e4985e8d16c98945
Adam Cohn <adamcohn88@gmail.com>
2018-08-21 19:57:57 -0400

    fixed precincts

--- app/models/county.rb
+++ app/models/county.rb
@@ -8,17 +8,18 @@ class County < ApplicationRecord
 
   def render_county_precint_results(office)
     formatted_hash = []
-    county_with_precincts = County.includes(:precincts).where(id: id).distinct
-    top_three = results.includes(:candidate).where(candidates: { office_id: office.id }).group('candidates.id').sum(:total).sort{|a,b| a[1]<=>b[1]}.reverse[0..2].map{|k, v| k }
-    candidate_results = results.includes(:precinct, :candidate).where(candidates: { office_id: office.id })
-    major_results = candidate_results.where(candidate_id: top_three).order('precincts.id').group(['precincts.id', 'candidates.id']).sum(:total)
-    other_results = candidate_results.where.not(candidate_id: top_three).order('precincts.id').group('precincts.id').sum(:total)
-    precinct_results = major_results.reduce({}){|v, (k, x)| v.merge!(k[0] => {k[1] => x}){|_, o, n| o.merge!(n)}}
-    other_results.delete_if { |k, v| !precinct_results.include?(k) }
-    precinct_results.keys.each do |precinct_id|
-      precinct_results[precinct_id] ||= [:other]
-      precinct_results[precinct_id][:other] ||= other_results[precinct_id].to_i
-      formatted_hash <<  Hash[name: county_with_precincts.first.precincts.find { |p| p.id == precinct_id }.name, results: precinct_results[precinct_id]]
+    statewide_total = Hash.new(0)
+    candidate_results = Result.where(state_id: state_id, office_id: office.id, county_id: id).group(['precinct_id', 'candidate_id']).sum(:total).reduce({}){|v, (k, x)| v.merge!(k[0] => {k[1] => x}){|_, o, n| o.merge!(n)}}
+    candidate_results.keys.each do |precinct_id|
+      candidate_results[precinct_id].each { |k, v| statewide_total[k] += v}
+    end
+    county_precincts = precincts
+    top_three = statewide_total.sort{|a,b| a[1]<=>b[1]}.reverse[0..2].to_h
+    candidate_results.keys.each do |precinct_id|
+      precinct_results = candidate_results[precinct_id].select { |k, v| top_three.keys.include?(k) }
+      other_precinct_results = candidate_results[precinct_id].select { |k, v| !top_three.keys.include?(k) }.values.inject(&:+)
+      precinct_results[:other] ||= other_precinct_results
+      formatted_hash << { id: precinct_id, name: county_precincts.find { |p| p.id == precinct_id }.name, results: precinct_results }
     end
     { results: formatted_hash }
   end
