51ce865a5f89fc6f0d342af462168bbf43784130
Adam Cohn <adamcohn88@gmail.com>
2018-08-24 10:37:38 -0400

    before more campaign finance additions

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
       get 'states/:state_id/offices/:office_id/results/district/:district_id', to: 'results#district_results'
       get 'states/:state_id/offices/:office_id/results/county', to: 'results#county_results'
       get 'states/:state_id/offices/:office_id/results/precinct/:county_id', to: 'results#precinct_results'
+
       # get 'results/export/county/:state_id', to: 'results#county_export'
       # get 'results/export/precinct/:state_id', to: 'results#precinct_export'
     end
